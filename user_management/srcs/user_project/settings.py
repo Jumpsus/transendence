30f69f7b35cf6351e9ee66ce56a81c13bd250721
Jumpsus <peerapat.rs@gmail.com>
2024-05-14 05:32:59 +0700

    [20240514] Allow cors in backend

--- user_management/srcs/user_project/settings.py
+++ user_management/srcs/user_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'user_app.apps.UserAppConfig',
 ]
 
@@ -48,7 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['https://*']
 
 ROOT_URLCONF = 'user_project.urls'
 
