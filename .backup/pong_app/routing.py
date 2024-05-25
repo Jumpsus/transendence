57a15f165dc087c3df978da96a8759e9a1139b48
Jumpsus <peerapat.rs@gmail.com>
2024-05-25 17:41:22 +0700

    delete unused file

--- .backup/pong_app/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-
-from . import consumers
-
-websocker_urlpatterns = [
-    re_path(r'^ws/pong/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
-]
--- backend/pong_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from pong_app import views
-
-urlpatterns = [
-    path('status', views.status, name='status'),
-    path("", views.index, name='index'),
-    path('test', views.test, name='test'),
-    path("<str:room_name>/", views.room, name="room"),
-]
