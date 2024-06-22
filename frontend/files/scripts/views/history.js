b16a30ba189f3c9a98c4ac5fac8e3ba56713b554
preed <leo.silveri@mail.ru>
2024-06-23 00:39:51 +0700

    remove table history fake entry

--- frontend/files/scripts/views/history.js
+++ frontend/files/scripts/views/history.js
@@ -10,19 +10,15 @@ export class MatchHistory extends Component {
 	<div id="match-history-table" class="d-flex justify-content-between w-100">
 	<div id="name-column">
 		<div>Opponent</div>
-		<div>Leo</div>
 	</div>
 	<div id="score-column">
 		<div>Score</div>
-		<div>1:11</div>
 	</div>
 	<div id="result-column">
 		<div>Result</div>
-		<div>Won</div>
 	</div>
 	<div id="date-column" class="d-none d-sm-block">
 		<div>Date</div>
-		<div>01-01-02</div>
 	</div>
 </div>
 		`;
@@ -31,12 +27,12 @@ export class MatchHistory extends Component {
 	}
 
 	async render() {
+		if (!document.getElementById("profileHeader")) new Profile();
+		super.render();
 		const nameColumn = document.getElementById("name-column");
 		const scoreColumn = document.getElementById("score-column");
 		const resultColumn = document.getElementById("result-column");
 		const dateColumn = document.getElementById("date-column");
-		if (!document.getElementById("profileHeader")) new Profile();
-		super.render();
 		makeLinkActive(document.getElementById("profileMenu"));
 		const resp = await fetch(`https://${host}/user-management/user/getmatchhistory`, {
 			method: "GET",
