da2f31fc655bf509dd2f058c7ceae7f5709266be
ychen <anthony.chen1226@gmail.com>
2024-06-25 15:41:01 +0700

    fix a bug in pong socket

--- pong_game/srcs/game/consumers/Game.py
+++ pong_game/srcs/game/consumers/Game.py
@@ -128,7 +128,7 @@ class GameConsumer(AsyncWebsocketConsumer):
 	async def disconnect(self, close_code):
 		active_connections = cache.get("game" + self.game_id, 0)
 
-		if active_connections >= 3:
+		if active_connections >= 3 and self.player_id == 0:
 			return
 		if close_code != 4101 and close_code != 4102:
 			await self.channel_layer.group_send(
